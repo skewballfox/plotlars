b1ef1092743f6137c5b04fb6b0d083bfecd2ec70
skewballfox <joshua.ferguson@cortwo.com>
2025-09-08 17:15:27 -0500

    testing unique_stable ordering

--- src/common/polar.rs
+++ src/common/polar.rs
@@ -8,7 +8,7 @@ pub(crate) trait Polar {
         let unique_groups = data
             .column(group_col)
             .unwrap()
-            .unique()
+            .unique_stable()
             .unwrap()
             .cast(&DataType::String)
             .unwrap();
